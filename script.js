37a1e1cafe32e3cc9fd573cbc2a88a609d6d31b3
Fairus In'am Pratama <fairusinam08@gmail.com>
2024-09-03 15:24:26 +0700

    change

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ let span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function () {
   modal.style.display = "block";
-  console.log("berhasil");
 };
 
 span.onclick = function () {
