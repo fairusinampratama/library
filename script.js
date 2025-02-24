75399f4d7192b3e17181d4bb301420e782541933
Fairus In'am Pratama <fairusinam08@gmail.com>
2025-02-24 23:16:30 +0700

    new style

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-let modal = document.getElementById("modal");
-let btn = document.getElementById("addBook");
-let span = document.getElementsByClassName("close")[0];
+const bookForm = document.getElementById("book-form");
+const bookList = document.getElementById("book-list");
+const buttonForm = document.getElementById("button-form");
+const bookReadTotal = document.getElementById("book-read-total");
+const bookUnreadTotal = document.getElementById("book-unread-total");
+const bookTotal = document.getElementById("book-total");
+const buttonDeleteAll = document.getElementById("button-delete-all");
 
-btn.onclick = function () {
-  modal.style.display = "block";
-};
-
-span.onclick = function () {
-  modal.style.display = "none";
-};
-
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
-
-const myLibrary = [];
+let library = [
+    new Book("One Piece", "Eiichiro Oda", 1050, true),
+    new Book("Attack on Titan", "Hajime Isayama", 139, false),
+    new Book("Naruto", "Masashi Kishimoto", 700, true),
+    new Book("Death Note", "Tsugumi Ohba", 108, false),
+    new Book("Demon Slayer", "Koyoharu Gotouge", 205, true),
+    new Book("Fullmetal Alchemist", "Hiromu Arakawa", 116, true),
+    new Book("Hunter x Hunter", "Yoshihiro Togashi", 400, false),
+    new Book("Bleach", "Tite Kubo", 686, true),
+    new Book("Jujutsu Kaisen", "Gege Akutami", 249, false)
+  ];
+  
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -25,82 +26,86 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-function addBookToLibrary(title, author, pages, read) {
-  const newBook = new Book(title, author, pages, read);
-  myLibrary.push(newBook);
-  displayBooks();
-}
+const updateTotals = () => {
+  bookReadTotal.textContent = library.filter(
+    (book) => book.read === true
+  ).length;
+  bookUnreadTotal.textContent = library.filter(
+    (book) => book.read === false
+  ).length;
+  bookTotal.textContent = library.length;
+};
 
-function displayBooks() {
-  const libraryContainer = document.querySelector('.card-book');
-  libraryContainer.innerHTML = "";
-
-  myLibrary.forEach((book, index) => {
-    const bookContainer = document.createElement('div');
-    bookContainer.classList.add('card-container');
-
-    bookContainer.innerHTML = `
-      <div class="card-title">${book.title}</div>
-      <div class="card-author">${book.author}</div>
-      <div class="card-page">${book.pages} Pages</div>
-      <div class="card-status">
-        <div class="${book.read ? 'read' : 'not-read'}" 
-             onclick="toggleReadStatus(${index})">
-          ${book.read ? 'Read' : 'Not Read'}
-        </div>
-      </div>
-      <div class="card-remove">
-        <div class="remove" onclick="removeBook(${index})">Remove</div>
-      </div>
-    `;
-
-    libraryContainer.appendChild(bookContainer);
-  });
-}
-
-function toggleReadStatus(index) {
-  myLibrary[index].read = !myLibrary[index].read;
-  displayBooks();
-}
-
-function removeBook(index) {
-  myLibrary.splice(index, 1);
-  displayBooks();
-}
-
-function handleFormSubmit(event) {
-  event.preventDefault();
-
-  const title = document.getElementById('card-title-add').value;
-  const author = document.getElementById('card-author-add').value;
-  const pages = document.getElementById('card-pages-add').value;
-  const read = document.getElementById('card-status-add').checked;
-
-  addBookToLibrary(title, author, pages, read);
-  closeModal();
-}
-
-function openModal() {
-  const modal = document.getElementById('modal');
-  modal.style.display = 'block';
-}
-
-function closeModal() {
-  const modal = document.getElementById('modal');
-  modal.style.display = 'none';
-}
-
-document.getElementById('addBook').addEventListener('click', openModal);
-
-document.querySelector('.form-add').addEventListener('submit', handleFormSubmit);
-
-document.querySelector('.close').addEventListener('click', closeModal);
-
-window.onclick = function(event) {
-  const modal = document.getElementById('modal');
-  if (event.target == modal) {
-    modal.style.display = 'none';
+const displayBooks = () => {
+  if (!library.length) {
+    bookList.innerHTML =
+      "<tr><td colspan='5' style='text-align:center;'>No books available</td></tr>";
+    updateTotals();
+    return;
   }
-}
 
-addBookToLibrary('Naruto', 'M. Kishimoto', 295, true);
+  bookList.innerHTML = library
+    .map(
+      (book, index) => `
+  <tr>
+    <td>
+        ${book.title}
+    </td>
+    <td>
+        ${book.author}
+    </td>
+    <td>
+        ${book.pages}
+    </td>
+    <td>
+        <span class="material-symbols-outlined read ${
+          book.read ? "check" : "close"
+        }" data-index="${index}">${book.read ? "check" : "close"}</span>
+    </td>
+    <td>
+        <span class="material-symbols-outlined delete" data-index="${index}">delete</span>
+    </td>
+  </tr>
+  `
+    )
+    .join("");
+  updateTotals();
+};
+
+const addBook = () => {
+  const formData = new FormData(bookForm);
+  const title = formData.get("title");
+  const author = formData.get("author");
+  const pages = formData.get("pages");
+  const read = formData.get("read") === "on" ? true : false;
+  library.push(new Book(title, author, pages, read));
+  bookForm.reset();
+  displayBooks();
+};
+
+const removeBook = (index) => {
+  library.splice(index, 1);
+};
+
+const toogleReadStatus = (index) => {
+  library[index].read = !library[index].read;
+};
+
+bookForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  addBook();
+});
+
+bookList.addEventListener("click", (event) => {
+  const index = Number(event.target.getAttribute("data-index"));
+  if (event.target.classList.contains("delete")) removeBook(index);
+  if (event.target.classList.contains("read")) toogleReadStatus(index);
+  displayBooks();
+});
+
+buttonDeleteAll.addEventListener("click", () => {
+  library = [];
+  displayBooks();
+});
+
+displayBooks();
